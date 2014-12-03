490ba38a752bd100f7ff44026b007e7b6d17b8c9
manana1995 <manana1995@gmail.com>
2014-12-03 14:25:50 +0300

    the folder with game from my local repository
    moving to team repository

---
+++ Tamagochi/Tamagochi/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tamagochi
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int money = 10;
+        public MainWindow()
+        {
+            InitializeComponent();
+            HealthBar.progressBar1.Value = 50;
+            HealthBar.label1.Content = "Health";
+            MoodBar.progressBar1.Value = 20;
+            MoodBar.label1.Content = "Mood";
+            HungerBar.progressBar1.Value = 70;
+            HungerBar.label1.Content = "Hunger";
+            labelMoney.Content = money + "$";
+        }
+    }
+}
---
+++ Tamagochi/Tamagochi/Parametr.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Tamagochi
+{
+    /// <summary>
+    /// Interaction logic for Parametr.xaml
+    /// </summary>
+    public partial class Parametr : UserControl
+    {
+        public Parametr()
+        {
+            InitializeComponent();
+        }
+
+        private void progressBar1_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            labelPercent.Content = progressBar1.Value + "%";
+            if (progressBar1.Value < 35)
+                progressBar1.Foreground = Brushes.DarkRed;
+            else
+                progressBar1.Foreground = Brushes.Green;
+        }
+    }
+}
